7651786c3add6d56f268467e07ad3760886a851a
afd <afd@alup5.academiapostal.es.local>
2020-08-11 16:57:58 +0200

    pab3980

---
+++ api/tareas.js
@@ -0,0 +1,77 @@
+const { Tarea } = require("../models")
+
+function crearTarea(req, res) {
+    Tarea.create(req.body)
+    .then(nuevaTarea => {
+        res.status(201).json(nuevaTarea)
+    })
+    .catch(err => {
+        res.status(400).json(err.message)
+    })
+}
+
+function listarTareas(req, res) {
+    Tarea.findAll()
+    .then(tareas => {
+        res.status(200).json(tareas)
+    })
+    .catch(err => {
+        res.status(400).json(err.tareas)
+    })
+}
+
+function leerTarea(req, res) {
+    Tarea.findByPk(req.params.id, {
+        include: [{model:Usuario, as:'participantes'}, Tarea]
+    })
+    .then(tarea => {
+        if (tarea) res.status(200).json(tarea)
+        else res.status(404).json("Tarea no encontrada")
+    })
+    .catch(err => {
+        res.status(400).json(err.message)
+    })
+}
+
+function modificarTarea(req, res) {
+    Tarea.findByPk(req.params.id)
+    .then(tarea => {
+        if (tarea) {
+            Object.assign(tarea, req.body)
+            tarea.save()
+            .then(tarea => {
+                res.status(200).json(tarea)
+            })
+        }
+        else res.status(404).json("Tarea no encontrada")
+    })
+    .catch(err => {
+        res.status(400).json(err.message)
+    })
+}
+
+function eliminarTarea(req, res) {
+    Tarea.findByPk(req.params.id)
+    .then(tarea => {
+        if (tarea) {
+            proyecto.destroy()
+            .then(() => {
+                res.status(200).json({})
+            })
+        } else {
+            res.status(404).json("Tarea no encontrada")
+        }
+    })
+    .catch(err => {
+        res.status(400).json(err.message)
+    })
+}
+
+
+module.exports = {
+    crearTarea,
+    listarTareas,
+    leerTarea,
+    modificarTarea,
+    eliminarTarea
+}
